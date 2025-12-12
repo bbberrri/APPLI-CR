2b45cba29b5a84ff09cbe3e968938768aa1f2a52
Bilal <bilal@berri.com>
2025-12-12 14:12:31 +0100

    Ajoutez des fichiers projet.

---
+++ APPLI-CR/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace APPLI_CR
+{
+    partial class FrmMenu
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+            this.gestionVisiteursToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rapportsVisiteursToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.menuStrip2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip2
+            // 
+            this.menuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gestionVisiteursToolStripMenuItem,
+            this.rapportsVisiteursToolStripMenuItem});
+            this.menuStrip2.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip2.Name = "menuStrip2";
+            this.menuStrip2.Size = new System.Drawing.Size(467, 24);
+            this.menuStrip2.TabIndex = 1;
+            this.menuStrip2.Text = "menuStrip2";
+            // 
+            // gestionVisiteursToolStripMenuItem
+            // 
+            this.gestionVisiteursToolStripMenuItem.Name = "gestionVisiteursToolStripMenuItem";
+            this.gestionVisiteursToolStripMenuItem.Size = new System.Drawing.Size(126, 20);
+            this.gestionVisiteursToolStripMenuItem.Text = "Gestion des visiteurs";
+            this.gestionVisiteursToolStripMenuItem.Click += new System.EventHandler(this.gererLesVisiteursToolStripMenuItem_Click);
+            // 
+            // rapportsVisiteursToolStripMenuItem
+            // 
+            this.rapportsVisiteursToolStripMenuItem.Name = "rapportsVisiteursToolStripMenuItem";
+            this.rapportsVisiteursToolStripMenuItem.Size = new System.Drawing.Size(112, 20);
+            this.rapportsVisiteursToolStripMenuItem.Text = "Rapports visiteurs";
+            this.rapportsVisiteursToolStripMenuItem.Click += new System.EventHandler(this.rapportsVisiteursToolStripMenuItem_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // FrmMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(467, 255);
+            this.Controls.Add(this.menuStrip2);
+            this.Name = "FrmMenu";
+            this.Text = "Menu";
+            this.menuStrip2.ResumeLayout(false);
+            this.menuStrip2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MenuStrip menuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem gestionVisiteursToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem rapportsVisiteursToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+    }
+}
+
---
+++ APPLI-CR/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace APPLI_CR
+{
+    public partial class FrmMenu : Form
+    {
+        public FrmMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void gererLesVisiteursToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rapportsVisiteursToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form f = new FrmGestionVisiteurs();
+            f.Show();
+        }
+    }
+}
